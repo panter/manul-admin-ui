4c6d62b1a6dc44c63b064053df011b4f4747acf2
remolueoend <remolueoend@users.noreply.github.com>
2017-05-16 16:00:31 +0200

    Add empty default modules

--- dist/manul-admin-ui.js
+++ dist/manul-admin-ui.js
@@ -1,7 +1,17 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = 5;
+
+var _adminUiModule = require('./admin-ui-module');
+
+var _getDefaultUi = require('./get-default-ui');
+
+exports.default = {
+  module: _adminUiModule.module,
+  getDefaultUI: _getDefaultUi.getDefaultUI,
+  containers: {},
+  components: {}
+};
 //# sourceMappingURL=manul-admin-ui.js.map
---
+++ src/manul-admin-ui.js
@@ -0,0 +1,9 @@
+import { module } from './admin-ui-module';
+import { getDefaultUI } from './get-default-ui';
+
+export default {
+  module,
+  getDefaultUI,
+  containers: {},
+  components: {},
+};
