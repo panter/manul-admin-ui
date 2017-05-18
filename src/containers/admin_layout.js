a0861661ba3e3074942b8f89b0a1707b894a1bdd
remolueoend <remolueoend@users.noreply.github.com>
2017-05-18 14:31:07 +0200

    Provide MainLayout to AdminLayout and NotAllowed components.

--- src/containers/admin_layout.js
+++ src/containers/admin_layout.js
@@ -7,12 +7,31 @@ export const composer = ({ context }, onData) => {
   onData(null, {});
 };
 
+/**
+ * Returns the main layout defined in the admin context.
+ * Returns false if not defined.
+ * @param {} context
+ */
+export const mainLayoutMapper = context =>
+  !!context &&
+  !!context.adminContext &&
+  !!context.adminContext.components &&
+  context.adminContext.components.mainLayout;
+
+/**
+ * NotAllowed wrapper providing a main layout reference.
+ */
+const NotAllowedWrapper = useDeps(context => ({
+  MainLayout: mainLayoutMapper(context),
+}))(NotAllowed);
+
 export const depsMapper = context => ({
   context: () => context,
+  MainLayout: mainLayoutMapper(context),
 });
 
 export default composeAll(
   composeWithTracker(composer),
-  // restrictToRoles('admin', () => NotAllowed),
+  restrictToRoles('admin', NotAllowedWrapper),
   useDeps(depsMapper),
 )(AdminLayout);
